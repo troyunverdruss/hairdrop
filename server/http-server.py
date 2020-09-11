a1b15bb73c46da4f7d3a40ad120f592724d899d7
Troy Unverdruss <troy@scroll.com>
2020-09-11 11:15:02 -0700

    file uploads with name working

--- server/http-server.py
+++ server/http-server.py
@@ -1,19 +1,22 @@
 from flask import Flask
 from flask import request
 from flask import make_response
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=("GET", "POST", "OPTIONS"))
-def anything():
-    with open("upload", "wb") as f:
-        f.write(request.get_data())
+@app.route('/upload/<filename>', methods=["POST", "OPTIONS"])
+def upload(filename):
+    if request.method == 'POST':
+        safe_filename = secure_filename(filename)
+        with open(safe_filename, "wb") as f:
+            f.write(request.get_data())
 
-    res = make_response('hi hi')
-    res.headers['Access-Control-Allow-Origin'] = '*'
-    res.headers['Access-Control-Allow-Headers'] = '*'
-    return res
+    response = make_response()
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    return response
 
 
 if __name__ == "__main__":
