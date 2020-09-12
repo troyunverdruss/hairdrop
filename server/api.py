68603579e60995b29e2a184b5c1835b65ec5be48
Troy Unverdruss <troy@scroll.com>
2020-09-11 19:51:55 -0700

    adding waitress to serve files

--- server/api.py
+++ server/api.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Blueprint, jsonify, make_response, request
 from flask_cors import cross_origin
 from werkzeug.utils import secure_filename
@@ -8,14 +10,19 @@ from models import Entry, EntryType
 database = Database()
 api = Blueprint('api', __name__)
 
+if 'upload_path' in os.environ:
+    upload_path = os.environ['UPLOAD_PATH']
+else:
+    upload_path = '/uploads'
 
-@api.route('/upload/<filename>', methods=["POST"])
+
+@api.route(f'{upload_path}/<filename>', methods=["POST"])
 @cross_origin()
 def upload(filename):
     safe_filename = secure_filename(filename)
     entry = Entry(type=EntryType.FILE, data=safe_filename)
     database.save(entry)
-    with open(f'uploads/{safe_filename}', "wb") as f:
+    with open(f'{upload_path}/{safe_filename}', "wb") as f:
         f.write(request.get_data())
     return ""
 
@@ -38,7 +45,7 @@ def list_entries():
 @api.route('/file/<filename>', methods=["GET"])
 @cross_origin()
 def get_file(filename):
-    with open(f'uploads/{filename}', "rb") as f:
+    with open(f'{upload_path}/{filename}', "rb") as f:
         response = make_response(f.read())
     response.headers['Content-Disposition'] = f'attachment; filename="{filename}"'
     return response
--- server/main.py
+++ server/main.py
@@ -2,10 +2,13 @@ from flask import Flask
 
 from api import api
 
+import waitress
+
 app = Flask(__name__)
 app.register_blueprint(api)
 
 # https://www.compose.com/articles/using-postgresql-through-sqlalchemy/
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    waitress.serve(app, port=8080)
+    # app.run(host="0.0.0.0", port=8080)
